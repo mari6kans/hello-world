99b18234b51b0a71b9f59a45d649c1a0b1558f98
mari6kans <mari6kans@gmail.com>
2015-09-21 10:57:13 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,4 +1,9 @@
-#include  <iostream>
-void main (){
-cout<< "Hello World!!!";
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+int main(void) {
+	 std::cout << "Hello world!!!";
+	return 0;
 }
+
